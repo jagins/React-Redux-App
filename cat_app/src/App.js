f426346847d7d9f85da0b87660c97f791e88638d
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-01-23 15:27:19 -0600

    redux connected to app and state is mapped to props

--- cat_app/src/App.js
+++ cat_app/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 
+import {connect} from 'react-redux';
+import {getCats} from './actions';
+
 function App() 
 {
   return (
@@ -10,4 +13,13 @@ function App()
   );
 }
 
-export default App;
+const mapStateToProps = state =>
+{
+  return{
+    isLoading: state.isLoading,
+    cats: state.cats,
+    error: state.error
+  };
+};
+
+export default connect(mapStateToProps, {getCats})(App);
