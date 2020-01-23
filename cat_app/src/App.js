443cd1d84e7f21d55f45140f3201a560b2d06179
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-01-23 14:50:33 -0600

    Added dependencies

--- cat_app/src/App.js
+++ cat_app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function App() 
+{
   return (
     <div className="App">
-     
+     <h1>Welcome to the Cat App</h1>
     </div>
   );
 }
